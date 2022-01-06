8fd4c31372b015d641d5538f7f2eebb94c83b2de
Imtiaz <imtiaz.emu.it@gmail.com>
2022-01-06 23:15:29 +0800

    added faker to generate seed data, commented seeds code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 %w[Admin Talent Fan Celebrity].each do |role|
   Role.where(name: role).first_or_create
 end
@@ -14,3 +16,74 @@ unless User.find_by(email: 'admin@bigshoutz.com')
 end
 
 puts('2. Admin User created')
+
+# Create test Users, Services, Listings to populate the database
+# ['Hangout', 'Live Video'].each do |service|
+#   Service.new(title: service, description: Faker::Lorem.paragraph).save!
+# end
+
+# 3.times do |i|
+#   user = User.new(email: "ce#{i+1}@bigshoutz.com", password: '123123123')
+#   user.skip_confirmation_notification!
+#   user.confirmed_at = DateTime.now
+#   user.save!
+#
+#   user.roles << Role.find_by(name: 'Celebrity')
+#
+#   listing = Listing.new(
+#       owner_id: user.id,
+#       service_id: Service.all.sample.id,
+#       name: Faker::Lorem.sentence,
+#       description: Faker::Lorem.paragraph,
+#       available_on: Time.now + 7.days,
+#       price: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+#       currency: Faker::Currency.code,
+#       meta_keywords: Faker::Lorem.words.join(','),
+#       event_time: Time.now + 10.days,
+#       event_place: Faker::Address.full_address
+#   )
+#
+#   listing.uploads.attach(
+#       io: File.open("/Users/imtiaz/adzymic/SampleImages/300x250/300x250_#{i+1}.jpg"),
+#       filename: "300x250_#{i+1}.jpg",
+#       content_type: 'image/jpg'
+#   )
+#
+#   listing.save!
+# end
+
+# 2.times do |i|
+#   user = User.new(email: "ta#{i+1}@bigshoutz.com", password: '123123123')
+#   user.skip_confirmation_notification!
+#   user.confirmed_at = DateTime.now
+#   user.save!
+#
+#   user.roles << Role.find_by(name: 'Talent')
+#
+#   listing = user.listings.new(
+#       service_id: Service.all.sample.id,
+#       name: Faker::Lorem.sentence,
+#       description: Faker::Lorem.paragraph,
+#       available_on: Time.now + 7.days,
+#       price: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+#       currency: Faker::Currency.code,
+#       meta_keywords: Faker::Lorem.words.join(','),
+#       event_time: Time.now + 10.days,
+#       event_place: Faker::Address.full_address
+#   )
+#
+#   listing.uploads.attach(
+#       io: File.open("/Users/imtiaz/adzymic/SampleImages/300x250/300x250_#{i+1}.jpg"),
+#       filename: "300x250_#{i+1}.jpg",
+#       content_type: 'image/jpg'
+#   )
+# end
+#
+# 4.times do |i|
+#   user = User.new(email: "fa#{i+1}@bigshoutz.com", password: '123123123')
+#   user.skip_confirmation_notification!
+#   user.confirmed_at = DateTime.now
+#   user.save!
+#
+#   user.roles << Role.find_by(name: 'Fan')
+# end
